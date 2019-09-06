3135bdaa3e558a4f8b9622e4f7b2fcd53a5442dc
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-09-06 16:53:56 -0300

    Exercicios C1E2

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,30 +6,26 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		String product1 = "Computer";
-		String product2 = "Office desk";
+		System.out.println("Enter your full name:");
+		String name = sc.nextLine();
+		System.out.println("How many bedrooms are there in your house?");
+		int bedrooms = sc.nextInt();
+		System.out.println("Enter product price:");
+		double price = sc.nextDouble();
+		System.out.println("Enter your last name, age and height (same line):");
+		String lastName = sc.next();
+		int age = sc.nextInt();
+		double height = sc.nextDouble();
 		
-		int age = 30;
-		int code = 5290;
-		char gender = 'F';
-		
-		double price1 = 2100.0;
-		double price2 = 650.50;
-		double measure = 53.234567;
-		
-		System.out.println("Products:");
-		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
-		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
-		System.out.println();
-		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
-		System.out.println();
-		System.out.printf("Measue with eight decimal places: %.8f%n", measure);
-		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
-		Locale.setDefault(Locale.US);
-		System.out.printf("US decimal point: %.3f%n", measure);
-		
+		System.out.println(name);
+		System.out.println(bedrooms);
+		System.out.printf("%.2f%n", price);
+		System.out.println(lastName);
+		System.out.println(age);
+		System.out.printf("%.2f%n", height);
 		
 		sc.close();
 	}
