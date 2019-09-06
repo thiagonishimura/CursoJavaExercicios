3ccabd8e96d3821ec268e94395d6f96034a4e506
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-09-06 18:38:30 -0300

    Exercicio C1E9

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -9,16 +9,23 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter number of students: ");
-		int n = sc.nextInt();
-		for (int i = 1; i <= n; i++) {
-			System.out.print("Student #" + i + ": ");
-			double score1 = sc.nextDouble();
-			double score2 = sc.nextDouble();
-			double score3 = sc.nextDouble();
-			double finalScore = score1 + score2 + score3;
-			System.out.printf("Final score: %.2f%n", finalScore);
-		}
+		double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
+
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+
+		triangulo = A * C / 2.0;
+		circulo = 3.14159 * C * C;
+		trapezio = (A + B) / 2.0 * C;
+		quadrado = B * B;
+		retangulo = A * B;
+
+		System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		System.out.printf("CIRCULO: %.3f%n", circulo);
+		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		System.out.printf("QUADRADO: %.3f%n", quadrado);
+		System.out.printf("RETANGULO: %.3f%n", retangulo);
 
 		sc.close();
 	}
