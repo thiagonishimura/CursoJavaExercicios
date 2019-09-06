f0fd6a2f70f6fc359b0c35a444961da1401be4fb
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-09-06 17:54:15 -0300

    Exercicio C1E5

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -8,19 +8,17 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter three student scores: ");
-		double nota1 = sc.nextDouble();
-		double nota2 = sc.nextDouble();
-		double nota3 = sc.nextDouble();
-		
-		double soma = nota1+nota2+nota3;
-		
-		System.out.printf("Final score = %.2f%n", soma);
-		if(soma<60.00) {
-			System.out.println("FAILED");
+
+		System.out.print("Enter glucose measure: ");
+		double glucose = sc.nextDouble();
+		if (glucose <= 100.0) {
+			System.out.println("Normal");
+		} else if (glucose <= 140.0) {
+			System.out.println("High");
+		} else {
+			System.out.println("Diabetes");
 		}
-			
+
 		sc.close();
 	}
 
